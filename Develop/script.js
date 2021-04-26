69bae89ba652716bcc797aa7f198fb412d67fbfd
Ody <melodymonyok@gmail.com>
2021-04-25 23:22:01 -0500

    Created initial repository and base code; needs a lot of work.

---
+++ Develop/script.js
@@ -0,0 +1,83 @@
+var emptyString = "";
+var alphabet = ["!", "@", "#", "$", "%", "^", "&", "*", "?"];
+var getRandomLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
+var randomLetter = getRandomLetter;
+
+console.log(getRandomLetter);
+
+/*
+// Assignment Code
+let generateBtn = document.querySelector("#generate")
+let specialCharList = ["!", "@", "#", "$", "%", "^", "&", "*", "?"]
+let passwordLength
+let lowerCaseChar
+let upperCaseChar
+let numericChar
+let specialChar
+
+document.getElementById("generate").onclick = function() {
+  passwordLength = prompt("Enter numeric length of password.");
+  lowerCaseChar = confirm("Do you want lowercase characters?");
+  upperCaseChar = confirm("Do you want UPPERCASE characters?");
+  numericChar = confirm("Do you want numeric characters?");
+  specialChar = confirm("Do you want special characters?");
+}
+
+// Will randomly generate the characters from each character set.
+getLowerCaseChar = function() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+getUpperCaseChar = function() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+getNumericChar = function() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+getSpecialChar = function() {
+  return
+}
+
+let specialCharacter = specialCharList[Math.floor(Math.random() * specialCharList.length)]
+
+console.log(specialCharacter);
+
+getSpecialChar = function() {
+  return specialCharList[Math.floor(Math.random() * specialCharList.length)];
+}
+
+getSpecialChar()*/
+
+/*
+randomLowerCaseLetter = random.nextInt(26);
+
+char value = 
+
+Math.floor()
+
+for (i = 0; i === passwordlength; */
+
+
+
+
+/*
+// Write password to the #password input
+function writePassword() {
+  let password = generatePassword();
+  let passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+
+
+
+function get.RandomLower() {
+
+}
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+*/
