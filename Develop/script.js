737d22a70c87a9f2c22852046b8b8b3e8c89e6b7
Ody <melodymonyok@gmail.com>
2021-04-28 22:42:27 -0500

    Got all of my random character generators made and my promts sorted out for the first part of choosing password criteria.

--- Develop/script.js
+++ Develop/script.js
@@ -1,9 +1,84 @@
-var emptyString = "";
-var alphabet = ["!", "@", "#", "$", "%", "^", "&", "*", "?"];
-var getRandomLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
-var randomLetter = getRandomLetter;
+// If I refresh the page, how do I make everything restart instead of stay in the middle?
+// How to I require that the passwordLength input be an integer?
 
-console.log(getRandomLetter);
+// These are my random character generating functions.
+getSpecialChar = function() {
+  let alphabet = ["!", "@", "#", "$", "%", "^", "&", "*", "?"];
+  let randomIndex = Math.floor(Math.random() * alphabet.length);
+  return alphabet[randomIndex];
+},
+getLowerCaseChar = function() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+},  
+getUpperCaseChar = function() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+},  
+getNumericChar = function() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+let lowerCaseChar = getLowerCaseChar();
+let upperCaseChar = getUpperCaseChar();
+let numericChar = getNumericChar();
+let specialChar = getSpecialChar();
+let charSet = [lowerCaseChar, upperCaseChar, numericChar, specialChar];
+
+// Testing to make sure random generators are working.
+getLowerCaseChar();
+console.log(lowerCaseChar);
+getUpperCaseChar();
+console.log(upperCaseChar);
+getNumericChar();
+console.log(numericChar);
+getSpecialChar();
+console.log(specialChar);
+
+document.getElementById("generate").onclick = function() {
+  passwordLength = prompt("Enter numeric length of password between 8 and 126.");
+  // This will ensure the user selects the correct password length.
+  while (passwordLength < 8) {
+    alert("Number entered is not in between 8 and 128!");
+    passwordLength = prompt("Enter numeric length of password between 8 and 126.");
+  }
+  lowerCaseChar = confirm("Do you want lowercase characters?");
+  upperCaseChar = confirm("Do you want UPPERCASE characters?");
+  numericChar = confirm("Do you want numeric characters?");
+  specialChar = confirm("Do you want special characters?");
+  // This will ensure the user selects at least one password criteria.
+  while (!lowerCaseChar && !upperCaseChar && !numericChar && !specialChar) {
+    alert("You must choose to use at least one option: lower case; upper case; number; and/or special character.")
+    lowerCaseChar = confirm("Do you want lowercase characters?");
+    upperCaseChar = confirm("Do you want UPPERCASE characters?");
+    numericChar = confirm("Do you want numeric characters?");
+    specialChar = confirm("Do you want special characters?");
+  };
+  // For internal logging use.
+  console.log("The password length is " + passwordLength);
+  console.log("They are using lower case letters: " + lowerCaseChar);
+  console.log("They are using upper case letters: " + upperCaseChar);
+  console.log("They are using numbers: " + numericChar);
+  console.log("They are using special characters: " + specialChar);
+  alert("Congratulations! You have sucessfuly selected your password criteria. Please wait while the password kittens assemble your password!")
+  generatePassword();
+  return;
+};
+
+/*
+function createPassword() {
+
+  generatePassword();
+  return;
+};
+createPassword(); */
+
+function generatePassword() {
+  if (lowerCaseChar) {
+    console.log(charSet[0]);
+  };
+  return;
+};
+password = generatePassword();
+
 
 /*
 // Assignment Code
@@ -15,13 +90,7 @@ let upperCaseChar
 let numericChar
 let specialChar
 
-document.getElementById("generate").onclick = function() {
-  passwordLength = prompt("Enter numeric length of password.");
-  lowerCaseChar = confirm("Do you want lowercase characters?");
-  upperCaseChar = confirm("Do you want UPPERCASE characters?");
-  numericChar = confirm("Do you want numeric characters?");
-  specialChar = confirm("Do you want special characters?");
-}
+
 
 // Will randomly generate the characters from each character set.
 getLowerCaseChar = function() {
@@ -36,9 +105,7 @@ getNumericChar = function() {
   return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
 }
 
-getSpecialChar = function() {
-  return
-}
+
 
 let specialCharacter = specialCharList[Math.floor(Math.random() * specialCharList.length)]
 
